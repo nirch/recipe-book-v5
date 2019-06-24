27e0350be7f8b9668f13102a634cc6e903f54428
nirch <nirh2@yahoo.com>
2019-06-24 10:58:41 +0300

    navbar ctrl + hard coded logic for isLoogedIn

---
+++ app/navbar/navbarCtrl.js
@@ -0,0 +1,8 @@
+
+app.controller("navbarCtrl", function($scope) {
+
+    $scope.isLoggedIn = function() {
+        return false;
+    }
+
+})
--- app/navbar/navbarDir.js
+++ app/navbar/navbarDir.js
@@ -2,7 +2,8 @@
 app.directive("recipeNavbar", function() {
 
     return {
-        templateUrl: "app/navbar/navbar.html"
+        templateUrl: "app/navbar/navbar.html",
+        controller: "navbarCtrl"
     }
 
 })
